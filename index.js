29a9c32abbc7b1692484ac4b76453c92348a5248
Alethea Toh <alethea.toh@duke.edu>
2018-04-09 01:30:03 +0800

    changes

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.get('*', (request, response) => {
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port '+PORT+' ~~~'));
 
 // Run clean up actions when server shuts down
 server.on('close', () => {
